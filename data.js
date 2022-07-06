836634e74373062be0d26d169cdde6c1d28ea221
Selga P. Muiznieks <selga.muiznieks@gmail.com>
2022-07-05 22:00:18 -0400

    Revert "db"
    This reverts commit bfdaa86610c51c0b9d434997bc455e1705162a6f.

--- data.js
+++ data.js
@@ -1,23 +1,30 @@
 // This is from the mongodb documentation on npmjs.com
 // https://www.npmjs.com/package/mongodb
-import { MongoClient } from "mongodb";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
 // Connection URL
 // const url = "mongodb://localhost:27017";
 // const url = "mongodb://127.0.0.1:27017";
-const url = "mongodb+srv://selga:d063NMIdf7mbSmmZ@cluster0.mw0ub5m.mongodb.net/?retryWrites=true&w=majority";
+// const url = "mongodb+srv://selga:d063NMIdf7mbSmmZ@cluster0.mw0ub5m.mongodb.net/?retryWrites=true&w=majority"
+// const client = new MongoClient(url);
+// // Database Name
+// const dbName = "to-do-list";
+// await client.connect();
+// console.log("Connected successfully to server");
+// const db = client.db(dbName);
+// const  collection = db.collection("tasks");
+const uri = "mongodb+srv://selga:d063NMIdf7mbSmmZ@cluster0.mw0ub5m.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {useUnifiedTopology: true});
 let db = null;
-// Database Name
-const dbName = "to-do-list";
-
-// UPDATED! THIS IS WORKING!!!
-MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client){
-  console.log("Connected successfully to server");
-  db = client.db(dbName);
+let collection = null;
+client.connect(err => {
+  db = client.db("to-do-list")
+  collection = db.collection("tasks");
+  // perform actions on the collection object
+  // client.close();
 });
 
 const getAllTasks = () => {
-  const collection = db.collection("tasks");
   return new Promise((resolve, reject) => {
     collection.find({}).toArray(function(err, tasks){
       err ? reject(err) : resolve(tasks);
@@ -26,7 +33,6 @@ const getAllTasks = () => {
 }
 
 const createTask = ({description, userId}) => {
-  const collection = db.collection("tasks");
   return new Promise((resolve, reject) => {
     collection.insertOne({
       userId,
