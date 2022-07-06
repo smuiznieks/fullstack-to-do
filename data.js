bfdaa86610c51c0b9d434997bc455e1705162a6f
Selga P. Muiznieks <selga.muiznieks@gmail.com>
2022-07-05 20:09:55 -0400

    db

--- data.js
+++ data.js
@@ -1,30 +1,23 @@
 // This is from the mongodb documentation on npmjs.com
 // https://www.npmjs.com/package/mongodb
-import { MongoClient, ServerApiVersion } from "mongodb";
+import { MongoClient } from "mongodb";
 
 // Connection URL
 // const url = "mongodb://localhost:27017";
 // const url = "mongodb://127.0.0.1:27017";
-// const url = "mongodb+srv://selga:d063NMIdf7mbSmmZ@cluster0.mw0ub5m.mongodb.net/?retryWrites=true&w=majority"
-// const client = new MongoClient(url);
-// // Database Name
-// const dbName = "to-do-list";
-// await client.connect();
-// console.log("Connected successfully to server");
-// const db = client.db(dbName);
-// const  collection = db.collection("tasks");
-const uri = "mongodb+srv://selga:d063NMIdf7mbSmmZ@cluster0.mw0ub5m.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {useUnifiedTopology: true});
+const url = "mongodb+srv://selga:d063NMIdf7mbSmmZ@cluster0.mw0ub5m.mongodb.net/?retryWrites=true&w=majority";
 let db = null;
-let collection = null;
-client.connect(err => {
-  db = client.db("to-do-list")
-  collection = db.collection("tasks");
-  // perform actions on the collection object
-  // client.close();
+// Database Name
+const dbName = "to-do-list";
+
+// UPDATED! THIS IS WORKING!!!
+MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client){
+  console.log("Connected successfully to server");
+  db = client.db(dbName);
 });
 
 const getAllTasks = () => {
+  const collection = db.collection("tasks");
   return new Promise((resolve, reject) => {
     collection.find({}).toArray(function(err, tasks){
       err ? reject(err) : resolve(tasks);
@@ -33,6 +26,7 @@ const getAllTasks = () => {
 }
 
 const createTask = ({description, userId}) => {
+  const collection = db.collection("tasks");
   return new Promise((resolve, reject) => {
     collection.insertOne({
       userId,
